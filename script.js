8a6cc48b178adc62dc0ec9de9c2e8b5531119e46
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-06-04 22:22:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-// // complete this js code
-// class Person {
-//     constructor(name, age) {
-//         this.name = name;
-//         this.age = age;
-//     }
-
-//     greet() {
-//         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-//     }
-// }
-
-// class Employee extends Person {
-//     constructor(name, age, jobTitle) {
-//         super(name, age);
-//         this.jobTitle = jobTitle;
-//     }
-
-//     jobGreet() {
-//         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-//     }
-// }
-
-// Person class
+// complete this js code
 class Person {
     constructor(name, age) {
         this.name = name;
@@ -33,7 +10,6 @@ class Person {
     }
 }
 
-// Employee class (inherits from Person)
 class Employee extends Person {
     constructor(name, age, jobTitle) {
         super(name, age);
@@ -44,6 +20,30 @@ class Employee extends Person {
         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
     }
 }
+
+// // Person class
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     greet() {
+//         console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+//     }
+// }
+
+// // Employee class (inherits from Person)
+// class Employee extends Person {
+//     constructor(name, age, jobTitle) {
+//         super(name, age);
+//         this.jobTitle = jobTitle;
+//     }
+
+//     jobGreet() {
+//         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+//     }
+// }
 
 // // Example usage
 // const person = new Person("Alice", 25);
